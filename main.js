f124c700f4e01f18d0bbc3675ac871f513cc08c8
Ashish Kumar <ashishkumar2294100@gmail.com>
2025-08-25 13:04:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var typed = new Typed('.text', {
+  strings:['FrontendDeveloper' , "Youtuber" , "Webdeveloper"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+      let menu = document.querySelector('#menu-icon');
+let nevbar = document.querySelector('.nevbar')
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  nevbar.classList.toggle('active');
+}
+window.onscroll = () => {
+   menu.classList.remove('bx-x');
+   nevbar.classList.remove('active');
+}
+let url='https://script.google.com/macros/s/AKfycbwYHabeNvglYYcZtkU71_EiRjh7vJJY8eNOqyeKTEEY7uokTg97rpxdD8A7IFqIKItt/exec'
+let form=document.querySelector('#form');
+    form=AggregateError("submit"),(e) =>
+{
+  let d= new FormData(form);
+  fetch(url,{
+    method:"POST",
+    body:d
+  }).then((res)=>res.text())
+  .then((finalRes)=>console.log(finalRes))
+  e.preventDefault();
+}
+
+
+
+
